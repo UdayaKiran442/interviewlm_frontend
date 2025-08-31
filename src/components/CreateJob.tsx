a84db4da568b73c3d18668a79639dd262e026280
UdayaKiran442 <gudaya2002@gmail.com>
2025-08-31 16:47:27 +0530

    create job UI refactoring

--- src/components/CreateJob.tsx
+++ src/components/CreateJob.tsx
@@ -1,18 +1,15 @@
 "use client"
 import { useState } from 'react';
-import { Plus, Info, Bot, Clock, Brain, Trash } from 'lucide-react'
+import { Plus, Info } from 'lucide-react'
 
 import { H3, H4, H5, Tagline } from "./ui/Typography";
 import { Input } from './ui/Input';
 import { Label } from './ui/Label';
 import { Card } from './ui/Card';
 import { IJobState, IRoundState } from '@/types/types';
-import { Select } from './ui/Select';
-import Button from './ui/Buttons';
+import { ButtonSecondary } from './ui/Buttons';
+import { Rounds } from './ui/Rounds';
 
-interface Props {
-    submitJob: (formData: FormData) => void;
-}
 
 export default function CreateJob() {
     const [jobDetails, setJobDetails] = useState<IJobState>({
@@ -112,10 +109,10 @@ export default function CreateJob() {
                         <div className='flex justify-between items-center'>
                             {/* title and button */}
                             <H5>Interview Rounds</H5>
-                            <button onClick={addRound} className="flex items-center gap-2 !bg-blue-600 hover:!bg-blue-700 text-white font-bold px-4 py-2 rounded-xl">
+                            <ButtonSecondary onClick={addRound} className="flex items-center gap-2">
                                 <Plus size={16} />
                                 Add Round
-                            </button>
+                            </ButtonSecondary>
                         </div>
                         {/* Tagline */}
                         <Tagline className='!-mt-2 !text-sm'>Design your interview process by adding and configuring different assessment stages.</Tagline>
@@ -128,110 +125,12 @@ export default function CreateJob() {
                         </div>
                         <div>
                             {/* Rounds info */}
-                            <Card className='border border-gray-300 rounded-2xl mt-2'>
-                                {rounds.length === 0 ?
-                                    <div className='flex flex-col justify-center items-center'>
-                                        <Clock size={24} color='gray' />
-                                        <Tagline>No interview rounds configured yet</Tagline>
-                                        <Tagline className='!text-[0.8rem] !flex !items-center !gap-1'>Click "Add Round" to get started</Tagline>
-                                    </div> :
-                                    rounds.map((round, index) => (
-                                        <div key={index} className="space-y-6">
-                                            <div className='flex justify-between items-center'>
-                                                <div className='flex items-center gap-2'>
-                                                    <Brain size={16} />
-                                                    <H5>Round {round.roundNumber}</H5>
-                                                </div>
-                                                <Trash className='cursor-pointer' size={16} color='red' />
-                                            </div>
-                                            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                                                <div className="space-y-2">
-                                                    <Label id='roundType' label='Interview Type' />
-                                                    <Select
-                                                        id='roundType'
-                                                        name='roundType'
-                                                        className='w-full border border-gray-300 rounded-lg p-3'
-                                                        value={round.roundType}
-                                                        onChange={(e) => updateRound(index, 'roundType', e.target.value)}
-                                                    >
-                                                        <option value="Technical Interview">Technical Interview</option>
-                                                        <option value="Behavioral Interview">Behavioral/HR/Culture fit Interview</option>
-                                                    </Select>
-                                                </div>
-                                                <div className="space-y-2">
-                                                    <Label id='roundName' label='Round Name' />
-                                                    <Input
-                                                        id='roundName'
-                                                        name='roundName'
-                                                        placeholder='e.g. Technical Interview - 1'
-                                                        type='text'
-                                                        value={round.roundName}
-                                                        onChange={(e) => updateRound(index, 'roundName', e.target.value)}
-                                                    />
-                                                </div>
-                                                <div className="space-y-2">
-                                                    <Label id='duration' label='Duration(in minutes)' />
-                                                    <Input
-                                                        id='duration'
-                                                        name='duration'
-                                                        placeholder='e.g. 60'
-                                                        type='number'
-                                                        value={round.duration}
-                                                        onChange={(e) => updateRound(index, 'duration', e.target.value)}
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                                                <div className="space-y-2">
-                                                    <Label id='difficulty' label='Difficulty' />
-                                                    <Select
-                                                        id='difficulty'
-                                                        name='difficulty'
-                                                        value={round.difficulty}
-                                                        onChange={(e) => updateRound(index, 'difficulty', e.target.value)}
-                                                    >
-                                                        <option value="easy">Easy</option>
-                                                        <option value="medium">Medium</option>
-                                                        <option value="hard">Hard</option>
-                                                    </Select>
-                                                </div>
-                                                <div className="space-y-2">
-                                                    <Label id='questionType' label='Question Type' />
-                                                    <Select
-                                                        id='questionType'
-                                                        name='questionType'
-                                                        value={round.questionType}
-                                                        onChange={(e) => updateRound(index, 'questionType', e.target.value)}
-                                                    >
-                                                        <option value="JD + Resume">JD + Resume</option>
-                                                        <option value="JD">JD</option>
-                                                    </Select>
-                                                </div>
-                                            </div>
-                                            <div className="flex items-center space-x-2 mt-4">
-                                                <input onChange={(e) => updateRound(index, 'isAI', e.target.checked)} type="checkbox" id="isAI" name="isAI" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
-                                                <Bot className="h-4 w-4 text-blue-500" />
-                                                <Label id="isAI" className='-ml-1.5' label="AI-Powered Interview" />
-                                            </div>
-                                            <div className="space-y-2">
-                                                <Label id='roundDescription' label='Round Description' />
-                                                <textarea
-                                                    id='roundDescription'
-                                                    className='w-full border border-gray-300 rounded-lg p-3'
-                                                    placeholder='Enter round description here'
-                                                    name='roundDescription'
-                                                    value={round.roundDescription ?? ""}
-                                                    onChange={(e) => updateRound(index, 'roundDescription', e.target.value)}
-                                                />
-                                            </div>
-                                        </div>
-                                    ))}
-                            </Card>
+                            <Rounds rounds={rounds} updateRound={updateRound} />
                         </div>
                     </div>
                 </Card>
                 <div className="mt-1.5">
-                    <Button onClick={submitJob} className='!bg-blue-600 !hover:bg-blue-700 !text-white !font-bold !px-4 !py-2'>Publish Job</Button>
+                    <ButtonSecondary onClick={submitJob}>Publish Job</ButtonSecondary>
                 </div>
             </div>
         </div>
--- src/components/ui/Buttons.tsx
+++ src/components/ui/Buttons.tsx
@@ -13,3 +13,18 @@ export default function Button({
     <button onClick={() => { onClick && onClick() }} className={cn("border-2 border-gray-200 rounded-xl px-10 py-2.5", className)}>{children}</button>
   );
 }
+
+
+export function ButtonSecondary({
+  children,
+  className,
+  onClick,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+}) {
+  return (
+    <button onClick={() => { onClick && onClick() }} className={cn("border-2 border-gray-200 !bg-blue-600 hover:!bg-blue-700 text-white font-bold px-4 py-2 rounded-xl", className)}>{children}</button>
+  );
+}
