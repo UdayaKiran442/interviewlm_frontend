0e42359c6f29d195b64e9bcaecc7f5cc40df1f4a
UdayaKiran442 <gudaya2002@gmail.com>
2025-09-01 13:28:28 +0530

    add loader to page hr

--- src/app/hr/create-job/page.tsx
+++ src/app/hr/create-job/page.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { Suspense } from "react";
 import CreateJob from "@/components/CreateJob";
 
 
-const HRPage: React.FC = () => {
-    return <CreateJob />;
+const CreateJobPage: React.FC = () => {
+    return <Suspense fallback={<div className="flex items-center justify-center h-screen">Loading...</div>}>
+        <CreateJob />
+    </Suspense>;
 };
 
-export default HRPage;
+export default CreateJobPage;
