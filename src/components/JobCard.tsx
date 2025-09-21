a64afebd3bae85f5cd71a3ca9490434cd2ea6daf
UdayaKiran442 <gudaya2002@gmail.com>
2025-09-21 11:46:51 +0530

    refactor jobs to job card component

---
+++ src/components/JobCard.tsx
@@ -0,0 +1,75 @@
+import { IJob } from "@/types/types";
+import React from "react";
+import ProfileIcon from "./ui/ProfileIcon";
+import { H5, Tagline } from "./ui/Typography";
+import IconWrapper from "./ui/IconWrapper";
+import { Building2, LocationEdit, Users, Wallet } from "lucide-react";
+import TitleCapsule from "./ui/TitleCapsule";
+import JobStats from "./JobStats";
+import { ButtonSecondary } from "./ui/Buttons";
+
+export default function JobCard({
+  job,
+  length,
+}: {
+  job: IJob;
+  length: number;
+}) {
+  return (
+    <div
+      key={job.jobId}
+      className="p-5 bg-gray-100 rounded-xl mt-2.5 border border-gray-200"
+    >
+      <div className="flex justify-between">
+        {/* title header */}
+        <div className="flex items-center gap-2">
+          {/* icon */}
+          <div>
+            <ProfileIcon name="BD" className="!w-14 !h-14 !text-2xl" />
+          </div>
+          {/* title and job details */}
+          <div>
+            <H5>{job.jobTitle}</H5>
+            <div className="flex gap-5">
+              <IconWrapper>
+                <Building2 size={20} />
+                <Tagline>{job.department}</Tagline>
+              </IconWrapper>
+
+              {job.location && (
+                <IconWrapper>
+                  <LocationEdit size={20} />
+                  <Tagline>{job.location}</Tagline>
+                </IconWrapper>
+              )}
+              {job.package && (
+                <IconWrapper>
+                  <Wallet size={20} />
+                  <Tagline>{job.package}</Tagline>
+                </IconWrapper>
+              )}
+            </div>
+            <div className="flex gap-5 mt-2">
+              <TitleCapsule
+                title={job.isJobOpen ? "Active" : "Closed"}
+                className="!px-3 !py-1 !text-sm"
+              />
+            </div>
+          </div>
+        </div>
+        <div>Posted on {job.createdAt.split("T")[0]}</div>
+      </div>
+      {/* stats */}
+      <div className="grid md:grid-cols-5 sm:grid-cols-2 mt-4">
+        <JobStats job={job} />
+      </div>
+      {/* actions */}
+      <div className="mt-5">
+        <ButtonSecondary className="flex items-center gap-2">
+          <Users size={20} />
+          View Applicants ({length})
+        </ButtonSecondary>
+      </div>
+    </div>
+  );
+}
--- src/components/JobsPage.tsx
+++ src/components/JobsPage.tsx
@@ -1,24 +1,13 @@
 "use client";
-import {
-  Briefcase,
-  Building2,
-  LocationEdit,
-  Plus,
-  Users,
-  Wallet,
-} from "lucide-react";
+import { Briefcase, Plus } from "lucide-react";
 
 import { IJob } from "@/types/types";
 import PageWrapper from "./ui/PageWrapper";
-import { H3, H4, H5, Tagline } from "./ui/Typography";
+import { H3, H4, Tagline } from "./ui/Typography";
 import { ButtonSecondary } from "./ui/Buttons";
-import ProfileIcon from "./ui/ProfileIcon";
-import IconWrapper from "./ui/IconWrapper";
-import TitleCapsule from "./ui/TitleCapsule";
-import JobStats from "./JobStats";
+import JobCard from "./JobCard";
 
 export default function JobsPage({ jobs }: { jobs: IJob[] }) {
-  console.log(jobs);
   return (
     <PageWrapper>
       <div className="p-8">
@@ -44,61 +33,7 @@ export default function JobsPage({ jobs }: { jobs: IJob[] }) {
             <H4>Job Postings ({jobs.length})</H4>
           </div>
           {jobs.map((job) => (
-            <div
-              key={job.jobId}
-              className="p-5 bg-gray-100 rounded-xl mt-2.5 border border-gray-200"
-            >
-              <div className="flex justify-between">
-                {/* title header */}
-                <div className="flex items-center gap-2">
-                  {/* icon */}
-                  <div>
-                    <ProfileIcon name="BD" className="!w-14 !h-14 !text-2xl" />
-                  </div>
-                  {/* title and job details */}
-                  <div>
-                    <H5>{job.jobTitle}</H5>
-                    <div className="flex gap-5">
-                      <IconWrapper>
-                        <Building2 size={20} />
-                        <Tagline>{job.department}</Tagline>
-                      </IconWrapper>
-
-                      {job.location && (
-                        <IconWrapper>
-                          <LocationEdit size={20} />
-                          <Tagline>{job.location}</Tagline>
-                        </IconWrapper>
-                      )}
-                      {job.package && (
-                        <IconWrapper>
-                          <Wallet size={20} />
-                          <Tagline>{job.package}</Tagline>
-                        </IconWrapper>
-                      )}
-                    </div>
-                    <div className="flex gap-5 mt-2">
-                      <TitleCapsule
-                        title={job.isJobOpen ? "Active" : "Closed"}
-                        className="!px-3 !py-1 !text-sm"
-                      />
-                    </div>
-                  </div>
-                </div>
-                <div>Posted on {job.createdAt.split("T")[0]}</div>
-              </div>
-              {/* stats */}
-              <div className="grid md:grid-cols-5 sm:grid-cols-2 mt-4">
-                <JobStats job={job} />
-              </div>
-              {/* actions */}
-              <div className="mt-5">
-                <ButtonSecondary className="flex items-center gap-2">
-                  <Users size={20} />
-                  View Applicants ({jobs.length})
-                </ButtonSecondary>
-              </div>
-            </div>
+            <JobCard job={job} length={jobs.length} key={job.jobId} />
           ))}
         </div>
       </div>
